8ab80115bf0030a8e7b02be3a5a163f10c8a1680
Xie Yuheng <xyheme@gmail.com>
2025-01-13 22:05:13 +0800

    remove function_ctx

--- src/lang/function_ctx.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct function_ctx_t {
-    hash_t *local_index_hash;
-};
-
-function_ctx_t *function_ctx_new(void);
-void function_ctx_destroy(function_ctx_t **self_pointer);
--- src/lang/index.h
+++ src/lang/index.h
@@ -5,5 +5,4 @@
 #include "call.h"
 #include "run.h"
 #include "execute.h"
-#include "function_ctx.h"
 #include "checks.h"
