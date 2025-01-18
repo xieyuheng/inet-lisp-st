af453546e64f4a9defe032ec0b68e7bd2abb7e72
Xie Yuheng <xyheme@gmail.com>
2025-01-18 20:10:32 +0800

    up

--- src/lang/stmt.c
+++ src/lang/stmt.c
@@ -6,5 +6,6 @@ stmt_define_function(char *name) {
     self->kind = STMT_DEFINE_FUNCTION;
     self->define_function.name = name;
     self->define_function.arg_name_list = list_new_with((destroy_fn_t *) string_destroy);
+    self->define_function.body = exp_list_new();
     return self;
 }
