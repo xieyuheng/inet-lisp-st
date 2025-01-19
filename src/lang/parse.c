297c21292a658a9d88820d976bc01f6cad2e90b0
Xie Yuheng <xyheme@gmail.com>
2025-01-19 15:45:52 +0800

    [lang] parse_stmt_list -- should not pop sexp

--- src/lang/parse.c
+++ src/lang/parse.c
@@ -9,8 +9,10 @@ parse_stmt(sexp_t *sexp) {
 list_t *
 parse_stmt_list(list_t *sexp_list) {
     list_t *stmt_list = list_new_with((destroy_fn_t *) stmt_destroy);
-    while (!list_is_empty(sexp_list)) {
-        list_push(stmt_list, parse_stmt(list_shift(sexp_list)));
+    sexp_t *sexp = list_first(sexp_list);
+    while (sexp) {
+        list_push(stmt_list, parse_stmt(sexp));
+        sexp = list_next(sexp_list);
     }
 
     return stmt_list;
