507995b6cacd2cc70055ea19e33747b9e74e1984
Xie Yuheng <xyheme@gmail.com>
2025-01-20 16:47:35 +0800

    [lang] parse_define_function

--- src/lang/parse.c
+++ src/lang/parse.c
@@ -16,10 +16,36 @@ parse_define_node(sexp_t *sexp) {
     return stmt_define_node(name, port_name_list);
 }
 
-static stmt_t *
-parse_define_function(sexp_t *sexp) {
+static exp_t *
+parse_exp(sexp_t *sexp) {
     (void) sexp;
     return NULL;
+}
+
+static stmt_t *
+parse_define_function(sexp_t *sexp) {
+    list_t *sexp_list = sexp_sexp_list(sexp);
+    (void) list_first(sexp_list);
+
+    list_t *name_sexp_list = sexp_sexp_list(list_next(sexp_list));
+    sexp_t *first_name_sexp = list_first(name_sexp_list);
+    char *name = string_copy(sexp_string(first_name_sexp));
+    (void) name;
+    list_t *arg_name_list = string_list_new();
+    sexp_t *name_sexp = list_next(name_sexp_list);
+    while (name_sexp) {
+        list_push(arg_name_list, string_copy(sexp_string(name_sexp)));
+        name_sexp = list_next(name_sexp_list);
+    }
+
+    list_t *exp_list = exp_list_new();
+    sexp_t *exp_sexp = list_next(sexp_list);
+    while (exp_sexp) {
+        list_push(exp_list, parse_exp(exp_sexp));
+        exp_sexp = list_next(sexp_list);
+    }
+
+    return stmt_define_function(name, arg_name_list, exp_list);
 }
 
 stmt_t *
