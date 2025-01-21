fd04a6b40b093d294ad85ca0568529fbb0a3b348
Xie Yuheng <xyheme@gmail.com>
2025-01-21 18:19:06 +0800

    [lang] execute -- handle stmt_define_function -- call stmt_print for testing

--- src/lang/execute.c
+++ src/lang/execute.c
@@ -4,11 +4,15 @@ void
 execute(vm_t *vm, stmt_t *stmt) {
     switch (stmt->kind) {
     case STMT_DEFINE_FUNCTION: {
+        stmt_print(stmt, stdout);
+        fprintf(stdout, "\n");
         // TODO
         return;
     }
 
     case STMT_DEFINE_NODE: {
+        stmt_print(stmt, stdout);
+        fprintf(stdout, "\n");
         define_node(
             vm->mod,
             stmt->define_node.name,
