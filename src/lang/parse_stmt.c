b748fe7f70a827d4b45db50082b5f6dd86eec169
Xie Yuheng <xyheme@gmail.com>
2025-01-21 17:20:35 +0800

    [lang] extract parse_exp.c & parse_stmt.c

--- src/lang/parse_stmt.c
+++ src/lang/parse_stmt.c
@@ -1,47 +1,5 @@
 #include "index.h"
 
-static exp_t *
-parse_var(sexp_t *sexp) {
-    return exp_var(string_copy(sexp_string(sexp)));
-}
-
-static exp_t *
-parse_ap(sexp_t *sexp) {
-    (void) sexp;
-    return NULL;
-    // list_t *sexp_list = sexp_sexp_list(sexp);
-    // assert(!list_is_empty(sexp_list));
-    // exp_t *target = parse_exp(list_first(sexp_list));
-    // list_t *arg_list = exp_list_new();
-    // return exp_ap(target, arg_list);
-}
-
-static exp_t *
-parse_assign(sexp_t *sexp) {
-    list_t *sexp_list = sexp_sexp_list(sexp);
-    (void) list_first(sexp_list);
-    list_t *name_list = string_list_new();
-    sexp_t *name_sexp = list_next(sexp_list);
-    while (name_sexp) {
-        list_push(name_list, string_copy(sexp_string(name_sexp)));
-        name_sexp = list_next(sexp_list);
-    }
-
-    return exp_assign(name_list);
-}
-
-exp_t *
-parse_exp(sexp_t *sexp) {
-    if (sexp_starts_with(sexp, "="))
-        return parse_assign(sexp);
-    else if (is_atom_sexp(sexp))
-        return parse_var(sexp);
-    else if (!list_is_empty(sexp_sexp_list(sexp)))
-        return parse_ap(sexp);
-    else
-        assert(false && "[parse_exp] can not handle empty list sexp");
-}
-
 static stmt_t *
 parse_define_node(sexp_t *sexp) {
     list_t *sexp_list = sexp_sexp_list(sexp);
