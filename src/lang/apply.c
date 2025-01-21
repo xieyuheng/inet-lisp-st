9f5c964c0af01e8a452118900e08594eef912a97
Xie Yuheng <xyheme@gmail.com>
2025-01-21 20:07:06 +0800

    up

--- src/lang/apply.c
+++ src/lang/apply.c
@@ -2,18 +2,15 @@
 
 void
 apply(vm_t *vm, value_t *target, size_t arity) {
-    (void) vm;
-    (void) target;
-    (void) arity;
-    // switch (def->kind) {
-    // case DEF_FUNCTION: {
-    //     apply_function(vm, def->function);
+    if (is_node(target)) {
+        apply_node(vm, as_node(target), arity);
+        return;
+    }
+
+    // if (is_function(target)) {
+    //     apply_function(vm, as_function(target), arity);
     //     return;
     // }
 
-    // case DEF_NODE: {
-    //     apply_node(vm, def->node, arity);
-    //     return;
-    // }
-    // }
+    assert(false && "[apply] unknown target");
 }
