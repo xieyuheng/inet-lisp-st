444903d46aebd9c44060fc6e73f5777751a991ce
Xie Yuheng <xyheme@gmail.com>
2025-01-22 20:02:15 +0800

    [lang] compile_exp_list

--- src/lang/compile.c
+++ src/lang/compile.c
@@ -9,9 +9,11 @@ compile_arg_name_list(vm_t *vm, function_t *function, list_t *arg_name_list) {
 
 void
 compile_exp_list(vm_t *vm, function_t *function, list_t *exp_list) {
-    (void) vm;
-    (void) function;
-    (void) exp_list;
+    exp_t *exp = list_first(exp_list);
+    while (exp) {
+        compile_exp(vm, function, exp);
+        exp = list_next(exp_list);
+    }
 }
 
 void compile_exp(vm_t *vm, function_t *function, exp_t *exp) {
