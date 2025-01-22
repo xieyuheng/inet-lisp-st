23b0a9d371de9159b6356a6a705eafa22e61ec17
Xie Yuheng <xyheme@gmail.com>
2025-01-22 20:10:45 +0800

    [lang] compile_arg_name_list

--- src/lang/compile.c
+++ src/lang/compile.c
@@ -3,8 +3,22 @@
 void
 compile_arg_name_list(vm_t *vm, function_t *function, list_t *arg_name_list) {
     (void) vm;
-    (void) function;
-    (void) arg_name_list;
+
+    function_ctx_t *ctx = function->ctx;
+    size_t index = hash_length(ctx->local_index_hash);
+    char *arg_name = list_last(arg_name_list);
+    while (arg_name) {
+        if (hash_has(ctx->local_index_hash, arg_name)) {
+            size_t old_index = (size_t) hash_get(ctx->local_index_hash, arg_name);
+            function_add_op(function, op_local_set(old_index));
+        } else {
+            hash_set(ctx->local_index_hash, arg_name, (void *) index);
+            function_add_op(function, op_local_set(index));
+            index++;
+        }
+
+        arg_name = list_prev(arg_name_list);
+    }
 }
 
 void
