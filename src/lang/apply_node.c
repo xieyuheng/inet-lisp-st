b261175c6e3994934e6f3d74612b07ecc9a8b804
Xie Yuheng <xyheme@gmail.com>
2025-01-22 20:36:29 +0800

    ambr input_arity arity

--- src/lang/apply_node.c
+++ src/lang/apply_node.c
@@ -1,10 +1,10 @@
 #include "index.h"
 
 static void
-node_apply_input_ports(vm_t *vm, node_t *node, size_t input_arity) {
-    for (size_t c = 0; c < input_arity; c++) {
+node_apply_input_ports(vm_t *vm, node_t *node, size_t arity) {
+    for (size_t c = 0; c < arity; c++) {
         wire_t *wire = stack_pop(vm->value_stack);
-        size_t i = input_arity - 1 - c;
+        size_t i = arity - 1 - c;
         wire->node = node;
         wire->index = i;
         node->wires[i] = wire;
@@ -14,8 +14,8 @@ node_apply_input_ports(vm_t *vm, node_t *node, size_t input_arity) {
 }
 
 static void
-node_return_output_ports(vm_t *vm, node_t *node, size_t input_arity) {
-    size_t output_arity = node->def->arity - input_arity;
+node_return_output_ports(vm_t *vm, node_t *node, size_t arity) {
+    size_t output_arity = node->def->arity - arity;
     for (size_t c = 0; c < output_arity; c++) {
         wire_t *node_wire = wire_new();
         wire_t *free_wire = wire_new();
@@ -23,7 +23,7 @@ node_return_output_ports(vm_t *vm, node_t *node, size_t input_arity) {
         node_wire->opposite = free_wire;
         free_wire->opposite = node_wire;
 
-        size_t i = input_arity + c;
+        size_t i = arity + c;
         node_wire->node = node;
         node_wire->index = i;
         node->wires[i] = node_wire;
@@ -33,8 +33,8 @@ node_return_output_ports(vm_t *vm, node_t *node, size_t input_arity) {
 }
 
 void
-apply_node(vm_t *vm, node_t *node, size_t input_arity) {
-    node_apply_input_ports(vm, node, input_arity);
-    node_return_output_ports(vm, node, input_arity);
+apply_node(vm_t *vm, node_t *node, size_t arity) {
+    node_apply_input_ports(vm, node, arity);
+    node_return_output_ports(vm, node, arity);
     return;
 }
