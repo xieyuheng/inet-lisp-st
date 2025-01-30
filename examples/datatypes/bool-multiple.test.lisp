d991f5694c6134b472f508fa6c15f7371bb32f8a
Xie Yuheng <xyheme@gmail.com>
2025-01-30 14:44:31 +0800

    [examples] bool-multiple.test.lisp

---
+++ examples/datatypes/bool-multiple.test.lisp
@@ -0,0 +1,26 @@
+(define-node t value!)
+(define-node f value!)
+
+(define-node conj x! y! z)
+
+(define-rule (conj (t) (t) z) (t z))
+(define-rule (conj (t) (f) z) (f z))
+(define-rule (conj (f) (t) z) (f z))
+(define-rule (conj (f) (f) z) (f z))
+
+(define-node disj x! y! z)
+
+(define-rule (disj (t) (t) z) (t z))
+(define-rule (disj (t) (f) z) (t z))
+(define-rule (disj (f) (t) z) (t z))
+(define-rule (disj (f) (f) z) (f z))
+
+(disj
+ (conj (conj (t) (t))
+       (conj (f) (t)))
+ (disj (disj (t) (t))
+       (disj (f) (t))))
+
+(wire-print-net)
+(debug)
+(wire-print-net)
