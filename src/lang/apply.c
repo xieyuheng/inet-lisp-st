ab8b986aab52dc91f2608240e943ccadebcad619
Xie Yuheng <xyheme@gmail.com>
2025-02-14 20:53:17 +0800

    up

--- src/lang/apply.c
+++ src/lang/apply.c
@@ -4,20 +4,13 @@ void
 apply(vm_t *vm, value_t target, size_t arity) {
     if (is_node_ctor(target)) {
         apply_node_ctor(vm, as_node_ctor(target), arity);
-        return;
-    }
-
-    if (is_function(target)) {
+    } else if (is_function(target)) {
         apply_function(vm, as_function(target), arity);
-        return;
-    }
-
-    if (is_primitive(target)) {
+    } else if (is_primitive(target)) {
         apply_primitive(vm, as_primitive(target), arity);
-        return;
+    } else {
+        fprintf(stderr, "[apply] unknown target: ");
+        value_print(target, stderr);
+        exit(1);
     }
-
-    fprintf(stderr, "[apply] unknown target: ");
-    value_print(target, stderr);
-    exit(1);
 }
