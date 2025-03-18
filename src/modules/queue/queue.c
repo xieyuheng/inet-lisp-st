9bf6a3c8642c3748f9709ea3f180f16cda490847
Xie Yuheng <xyheme@gmail.com>
2025-03-18 21:12:44 +0800

    [queue] `queue_is_full`

--- src/modules/queue/queue.c
+++ src/modules/queue/queue.c
@@ -56,15 +56,24 @@ queue_new_with(size_t size, destroy_fn_t *destroy_fn) {
     return self;
 }
 
+static cursor_t
+queue_next_back_cursor(const queue_t *self) {
+    return (self->back_cursor + 1) % self->size;
+}
+
+bool
+queue_is_full(const queue_t *self) {
+    return queue_next_back_cursor(self) == self->front_cursor;
+}
+
 void
 queue_enqueue(queue_t *self, void *value) {
-    cursor_t next_back_cursor = (self->back_cursor + 1) % self->size;
-    if (next_back_cursor == self->front_cursor) {
+    if (queue_is_full(self)) {
         // - `back_cursor` must not catch `front_cursor` from behind
         fprintf(stderr, "[queue_enqueue] the queue is full\n");
         exit(1);
     }
 
     self->values[self->back_cursor] = value;
-    self->back_cursor = next_back_cursor;
+    self->back_cursor = queue_next_back_cursor(self);
 }
