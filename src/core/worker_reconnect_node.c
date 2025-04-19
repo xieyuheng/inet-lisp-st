88dc095010842e8ee1b965a3285b9013c8262810
Xie Yuheng <xyheme@gmail.com>
2025-04-19 22:20:20 +0800

    up

--- src/core/worker_reconnect_node.c
+++ src/core/worker_reconnect_node.c
@@ -1,15 +1,12 @@
 #include "index.h"
 
 static void
-node_take_input(worker_t *worker, node_t *node, size_t index, value_t value) {
-    (void) worker;
+reconnect_input(node_t *node, size_t index, value_t value) {
     node_set_value(node, index, value);
 }
 
 static value_t
-node_return_output(worker_t *worker, node_t *node, size_t index) {
-    (void) worker;
-
+reconnect_output(node_t *node, size_t index) {
     wire_t *node_wire = wire_new();
     wire_t *free_wire = wire_new();
 
@@ -29,13 +26,13 @@ worker_reconnect_node(worker_t *worker, node_t *node, size_t arity) {
     for (size_t count = 0; count < arity; count++) {
         size_t index = arity - 1 - count;
         value_t value = stack_pop(worker->value_stack);
-        node_take_input(worker, node, index, value);
+        reconnect_input(node, index, value);
     }
 
     size_t output_arity = node->ctor->arity - arity;
     for (size_t count = 0; count < output_arity; count++) {
         size_t index = arity + count;
-        value_t value = node_return_output(worker, node, index);
+        value_t value = reconnect_output(node, index);
         stack_push(worker->value_stack, value);
     }
 
